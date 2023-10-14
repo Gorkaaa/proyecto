7a9c386b13bd381e0be0b60b0df6559998f3be85
Omar Eiyana <omar.e@opendeusto.es>
2023-10-14 15:07:26 +0200

    Añadir estructura de carpetas del proyeco

--- gaoMarket/src/gui/VentanaPrincipal.java
+++ gaoMarket/src/gui/VentanaPrincipal.java
@@ -1,9 +1,36 @@
 package gui;
 
-public class VentanaPrincipal {
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class VentanaPrincipal extends JFrame{	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public VentanaPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(640, 480);
+		setTitle("GAOmarket");
+		
+		
+		
+		
+		setVisible(true);
+	}
+	
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				new VentanaPrincipal();
+			}
+			
+		});
 
 	}
 
