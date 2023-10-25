779145ddaed571654199ccf1838915944e88087a
Gorkaaa <gorka.bidaurratzaga@opendeusto.es>
2023-10-25 18:44:46 +0200

    Añadidas 10 clases al paquete domain, de las cuales 3 son enum. Clases con sus atributos y contructores menos las clases  Persona, Empleado y Usuario.

---
+++ gaoMarket/src/domain/HigieneYBelleza.java
@@ -0,0 +1,43 @@
+package domain;
+
+public class HigieneYBelleza extends Producto {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected TipoHigieneYBelleza tipoHigieneYBelleza;
+	
+	public HigieneYBelleza(String nombre, String descripcion, String imagen, Double precio, int cantidad,
+			TipoHigieneYBelleza tipoHigieneYBelleza) {
+		super(nombre, imagen, precio, cantidad);
+		this.tipoHigieneYBelleza = tipoHigieneYBelleza;
+	}
+
+	public HigieneYBelleza() {
+		super();
+		this.tipoHigieneYBelleza = TipoHigieneYBelleza.AFEITADO_DEPILACION;
+	}
+
+	public TipoHigieneYBelleza getTipoHigieneYBelleza() {
+		return tipoHigieneYBelleza;
+	}
+
+	public void setTipoHigieneYBelleza(TipoHigieneYBelleza tipoHigieneYBelleza) {
+		this.tipoHigieneYBelleza = tipoHigieneYBelleza;
+	}
+
+	@Override
+	public String toString() {
+		return "HigieneYBelleza [tipoHigieneYBelleza=" + tipoHigieneYBelleza + ", id=" + id + ", nombre=" + nombre
+				+ ", imagen=" + imagen + ", precio=" + precio + ", cantidad=" + cantidad + "]";
+	}
+
+	
+	
+
+	
+	
+	
+}
