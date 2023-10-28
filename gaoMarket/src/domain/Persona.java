f047b3f91a8ec7cf338bada237fabad74be59c65
Gorkaaa <gorka.bidaurratzaga@opendeusto.es>
2023-10-28 18:00:49 +0200

    Añadir atributos a la clase Persona y hacerla que sea abstract con 2 metodos: verificarUsuario y estaRegistrado.

--- gaoMarket/src/domain/Persona.java
+++ gaoMarket/src/domain/Persona.java
@@ -1,18 +1,111 @@
 package domain;
 
-public class Persona {
-	protected String nobre;
-	protected String Apellidos;
+import java.util.Objects;
+
+abstract public class Persona {
+	protected String nombre;
+	protected String apellidos;
+	protected String nomUsuario;
 	protected int numTelefono;
 	protected String correoElectronico;
-	protected boolean registrado;
+	protected String contraseña;
 	
+	public Persona(String nombre, String apellidos, String nomUsuario, int numTelefono, String correoElectronico,
+			String contraseña) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.nomUsuario = nomUsuario;
+		this.numTelefono = numTelefono;
+		this.correoElectronico = correoElectronico;
+		this.contraseña = contraseña;
+	}
 	
-	
-	
-	
-	
-	
+	public Persona() {
+		super();
+		this.nombre = "";
+		this.apellidos = "";
+		this.nomUsuario = "";
+		this.numTelefono = 0;
+		this.correoElectronico = "";
+		this.contraseña = "";
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getNomUsuario() {
+		return nomUsuario;
+	}
+
+	public void setNomUsuario(String nomUsuario) {
+		this.nomUsuario = nomUsuario;
+	}
+
+	public int getNumTelefono() {
+		return numTelefono;
+	}
+
+	public void setNumTelefono(int numTelefono) {
+		this.numTelefono = numTelefono;
+	}
+
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellidos=" + apellidos + ", nomUsuario=" + nomUsuario
+				+ ", numTelefono=" + numTelefono + ", correoElectronico=" + correoElectronico + ", contraseña="
+				+ contraseña + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(correoElectronico, nomUsuario, numTelefono);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return Objects.equals(correoElectronico, other.correoElectronico)
+				&& Objects.equals(nomUsuario, other.nomUsuario) && numTelefono == other.numTelefono;
+	}	
+	
+	public abstract boolean verificarUsuario(String nomUsuario, String contraseña);
+	
+	public abstract boolean estaRegistrado();
 
 }
