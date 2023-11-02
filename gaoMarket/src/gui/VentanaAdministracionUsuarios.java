b00c32b074561150db689bdbea039ef2d6f9b8db
Omar Eiyana <omar.e@opendeusto.es>
2023-11-02 14:36:10 +0100

    Modicacion ventanaAdministacionUsuario cambio de JList a JTable

--- gaoMarket/src/gui/VentanaAdministracionUsuarios.java
+++ gaoMarket/src/gui/VentanaAdministracionUsuarios.java
@@ -1,28 +1,23 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.ButtonGroup;
-import javax.swing.DefaultListModel;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
-import javax.swing.ListCellRenderer;
-import javax.swing.ListSelectionModel;
-import javax.swing.SwingUtilities;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
 
-import domain.Empleado;
 import domain.GestorMarket;
 import domain.Usuario;
 
@@ -34,71 +29,28 @@ public class VentanaAdministracionUsuarios extends JFrame{
 	private static final long serialVersionUID = 1L;
 	
 	protected GestorMarket gestor;
+	protected List<Usuario> usuarios;
 	
 	public VentanaAdministracionUsuarios(GestorMarket gestor) {
-		
-		
 		this.setLayout(new BorderLayout());
-		
-		DefaultListModel<Usuario> modeloUsuarios = new DefaultListModel<>();
-		DefaultListModel<Empleado> modeloEmpleados = new DefaultListModel<>();
+				
 		//Prueba
-		List<Usuario> usuarios = new ArrayList<>();
+		usuarios = new ArrayList<>();
 		usuarios.add(new Usuario("Nombre1", "Apellido1", "usu1", 666666666, "correo@gmail.com", "contrasenya"));
 		usuarios.add(new Usuario("Nombre2", "Apellido2", "usu2", 666666666, "correo@gmail.com", "contrasenya"));
 		usuarios.add(new Usuario("Nombre3", "Apellido3", "usu3", 666666666, "correo@gmail.com", "contrasenya"));
 		usuarios.add(new Usuario("Nombre4", "Apellido4", "usu4", 666666666, "correo@gmail.com", "contrasenya"));
-		modeloUsuarios.addAll(usuarios);
 		
 		/*
+		DefaultListModel<Empleado> modeloEmpleados = new DefaultListModel<>();
 		List<Empleado> empleados = new ArrayList<>();
 		empleados.add(new Empleado("Nombre1", "Apellido1", "usu1", 666666666, "correo@gmail.com", "contrasenya", "00000000A"));
 		empleados.add(new Empleado("Nombre2", "Apellido2", "usu2", 666666666, "correo@gmail.com", "contrasenya", "00000000A"));
 		empleados.add(new Empleado("Nombre3", "Apellido3", "usu3", 666666666, "correo@gmail.com", "contrasenya", "00000000A"));
 		empleados.add(new Empleado("Nombre4", "Apellido4", "usu4", 666666666, "correo@gmail.com", "contrasenya", "00000000A"));
-		modeloEmpleados.addAll(empleados);*/
-
-		JList<Usuario> lstUsuarios = new JList<>(modeloUsuarios);
+		modeloEmpleados.addAll(empleados);
 		
-		//la lista solamente admite selección sencilla
-		lstUsuarios.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		
-		//Se establece el renderer para los elementos de la lista
-		class MyCellRenderer extends JLabel implements ListCellRenderer<Usuario>{
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public Component getListCellRendererComponent(JList<? extends Usuario> list, Usuario value, int index,
-					boolean isSelected, boolean cellHasFocus) {
-				//Se establece el valor del texto mostrado en el JLabel de la celda
-	            setText(value.toString());
-
-	            
-				// Cuando se seleccione un usuario cambia el color de la celda
-	            if (isSelected) {
-	            	setOpaque(true);
-	                setBackground(Color.cyan);
-	            }
-	            else
-	            	setOpaque(false);
-
-	            // se devuelve el componente visual. siempre se devuelve this
-	            // porque se evita crear uno nuevo para cada elemento de la lista
-	            return this;
-			}
-			
-		}
-		lstUsuarios.setCellRenderer(new MyCellRenderer());
-		
-		
-				
-		JScrollPane scrollPane = new JScrollPane(lstUsuarios);
-		add(scrollPane, "Center");
-		
-		/*JRadioButton rb_Usuarios = new JRadioButton("Usuarios");
+		JRadioButton rb_Usuarios = new JRadioButton("Usuarios");
 		JRadioButton rb_Empleados = new JRadioButton("Empleados");
 
         ButtonGroup grupoBotones = new ButtonGroup();
@@ -110,20 +62,130 @@ public class VentanaAdministracionUsuarios extends JFrame{
         panelIzq.add(rb_Empleados);
         add(panelIzq, "West");*/
 		
+		class ModeloTabla extends AbstractTableModel {
+			
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+			
+			String[] cabeceras = { "Nombre", "Apellidos", "Nombre Usuario", "Teléfono", "Correo", "Contraseña"};
+				
+			public ModeloTabla(List<Usuario> usuarios) {
+				VentanaAdministracionUsuarios.this.usuarios = usuarios;
+			}
+
+			@Override
+			public Object getValueAt(int row, int column) {
+				Usuario u = usuarios.get(row);
+				switch (column) {
+					case 0: return u.getNombre();
+					case 1: return u.getApellidos();
+					case 2: return u.getNomUsuario();
+					case 3: return u.getNumTelefono();
+					case 4: return u.getCorreoElectronico();
+					case 5: return u.getContrasenya();
+				}
+				return null;
+			}
+
+			@Override
+			public int getRowCount() {
+				return usuarios.size();
+			}
+
+			@Override
+			public int getColumnCount() {
+				return cabeceras.length;
+			}
+
+			@Override
+			public String getColumnName(int column) {
+				return cabeceras[column];
+			}
+			
+	        @Override
+	        public boolean isCellEditable(int row, int column) {
+	            return false;
+	        }
+	        
+	        @Override
+	        public void setValueAt(Object value, int row, int column) {
+	            Usuario u = usuarios.get(row);
+	            switch (column) {
+	                case 0:
+	                    u.setNombre((String) value);
+	                    break;
+	                case 1:
+	                    u.setApellidos((String) value);
+	                    break;
+	                case 2:
+	                    u.setNomUsuario((String) value);
+	                    break;
+	                case 3:
+	                    u.setNumTelefono((int) value);
+	                    break;
+	                case 4:
+	                    u.setCorreoElectronico((String) value);
+	                    break;
+	                case 5:
+	                    u.setContrasenya((String) value);
+	                    break;
+	            }
+	            
+	            fireTableCellUpdated(row, column);
+	        }
+		}
+		
+		JTextField buscador = new JTextField(20);
+		
+		class RendererTabla extends JLabel implements TableCellRenderer{
+
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+					boolean hasFocus, int row, int column) {
+				String cellText = value.toString();
+				
+				//Pone en negrita el string que aparece en las celdas
+				if(!buscador.getText().isBlank() && cellText.startsWith(buscador.getText())) {
+					setText(String.format("<html><b>%s</b>%s</htnml>",
+							buscador.getText(),
+							cellText.substring(buscador.getText().length())
+							));
+				}else {
+					setText(cellText);
+				}
+				
+				return this;
+			}
+			
+		}
+		
+		ModeloTabla modeloTabla = new ModeloTabla(usuarios);
+		JTable tablaUsuarios = new JTable(modeloTabla);
+		tablaUsuarios.setDefaultRenderer(Object.class, new RendererTabla());
+		JScrollPane scrollPane = new JScrollPane(tablaUsuarios);
+		add(scrollPane, "Center");
+		
 		JButton botonBorrar = new JButton("Eliminar Usuario");
-		botonBorrar.addActionListener(new ActionListener() {
+		/*botonBorrar.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(!lstUsuarios.isSelectionEmpty()) {
-					int usuarioSeleccionado = lstUsuarios.getSelectedIndex();
+				if(!tablaUsuarios.isSelectionEmpty()) {
+					int usuarioSeleccionado = tablaUsuarios.getSelectedIndex();
 					int confirmacion = JOptionPane.showConfirmDialog(null, "¿Estás seguro de que quieres eliminar el usuario?", "Confirmación de eliminación", JOptionPane.YES_NO_OPTION);
 					if (confirmacion == JOptionPane.YES_OPTION) {
 						modeloUsuarios.remove(usuarioSeleccionado);
 					}
 				}
 			}
-		});
+		});*/
 		
 		JButton botonAñadirUsuario = new JButton("Añadir Usuario");
 		botonAñadirUsuario.addActionListener(new ActionListener() {
@@ -141,8 +203,7 @@ public class VentanaAdministracionUsuarios extends JFrame{
 
 		add(panelBotones, "South");
 		
-		
-		this.setTitle("Lista de usuarios/empleados");
+		this.setTitle("Lista de usuarios");
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.setSize(1000, 480);
 		this.setVisible(false);
