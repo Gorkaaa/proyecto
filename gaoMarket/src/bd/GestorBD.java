f28a25af0a873ac1c95db2da3be7a5a77c3541d7
Omar Eiyana <omar.e@opendeusto.es>
2023-11-15 23:38:09 +0100

    Update: funcion que realiza una consulta para buscar un usuario en la bbdd

--- gaoMarket/src/bd/GestorBD.java
+++ gaoMarket/src/bd/GestorBD.java
@@ -2,19 +2,23 @@ package bd;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 
 import domain.GestorMarket;
+import domain.Usuario;
 
 public class GestorBD {
 	private GestorMarket gestor;
 	private Connection conn;
+	private Statement st;
 	
 	public GestorBD(GestorMarket gestor){
 		
 	}
-	public void cargarDriverBD() {
+	public GestorBD() {
 		// Carga del Driver de SQLite
 		try {
 			Class.forName("org.sqlite.JDBC");
@@ -23,17 +27,49 @@ public class GestorBD {
 		}
 	}
 	
-	public void conexionBD() {
+	/*public void conexionBD() {
 		try {
 			conn = DriverManager.getConnection("jdbc:sqlite:resources/db/GAOmarket.db");
+			st = conn.createStatement();
 			
 			// Consultas, busquedas, etc
 			
 			
+			st.close();
 			conn.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}*/
+	
+	public ResultSet realizarQuery(String sql) {
+		ResultSet rs = null;
+		try {
+			rs = st.executeQuery(sql);
+		} catch (SQLException e) {
+			gestor.getLogger().log(Level.SEVERE, "No se ha podido realizar la consulta " + sql);
+		}
+		return rs;
 	}
 	
+	// Metodo que busca un usuario en la BD por su email y contraseña
+	public Usuario buscaUsuario(String email, String password) { 
+		Usuario u = null;
+		 try{                  		         
+				conn = DriverManager.getConnection("jdbc:sqlite:resources/db/GAOmarket.db");
+	        	String sql = "SELECT * FROM usuario WHERE email = '" + email + "' AND contrasena = '"+ password +"'";
+	        	st = conn.createStatement();
+	            ResultSet rs = realizarQuery(sql);           
+	            if(rs.next()) { 
+	            	u = new Usuario(rs.getString("nombre"), rs.getString("apellido"), rs.getString("nomUsuario"), rs.getInt("telefono"), rs.getString("email"), rs.getString("contrasena"));
+	            }
+	            rs.close();
+	            st.close();
+	            conn.close();
+	         }
+	         catch (Exception e)  {
+	 			gestor.getLogger().log(Level.SEVERE, "Error en buscaUsuario(Email, password): " + e);
+	         } 
+		 return u;
+	}
 }
