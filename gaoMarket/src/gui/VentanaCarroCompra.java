7c6a621c034241c58f1bc673618cc3d299334e18
Gorkaaa <gorka.bidaurratzaga@opendeusto.es>
2023-12-12 11:19:40 +0100

    Añadida la funcionalidad de la VentanaCarroCompra con sus atributos y metodos.

--- gaoMarket/src/gui/VentanaCarroCompra.java
+++ gaoMarket/src/gui/VentanaCarroCompra.java
@@ -5,17 +5,20 @@ import java.awt.Container;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 import domain.GestorMarket;
 import domain.Producto;
+import domain.TipoProducto;
 
 public class VentanaCarroCompra extends JFrame{
 
@@ -25,23 +28,46 @@ public class VentanaCarroCompra extends JFrame{
 	private static final long serialVersionUID = 1L;
 	
 	protected GestorMarket gestor;
-	protected DefaultListModel<Producto> modeloCarrito;
+	protected List<Producto> productos;
+	protected JTable tabla;
+	protected JScrollPane tablaScroll;
+	protected ModeloCarroCompra modeloCarrito;
+	protected RendererCarroCompra rendererCompra;
 	protected JList<Producto> lstCarrito;
 	protected JButton btnEliminar;
 	protected JButton btnComprar;
 	protected JButton btnVaciar;
+	protected JPanel panelIzq;
+	protected JPanel panelMedio;
+	protected JPanel panelArriba;
+	protected JPanel panelAbajo;
     
 	public VentanaCarroCompra(GestorMarket gestor) {
 		this.gestor = gestor;
 		Container cp = this.getContentPane();
 		
 		this.setLayout(new BorderLayout());
+		productos = new ArrayList<Producto>();
 		
-		modeloCarrito = new DefaultListModel<>();
-		lstCarrito = new JList<>(modeloCarrito);
+		productos.add(new Producto(1, "Producto 1", "imagen1.png", 19.99, 2, TipoProducto.ALIMENTO));
+		productos.add(new Producto(5, "Producto 5", "imagen5.png", 4.20, 6, TipoProducto.ALIMENTO));
+		productos.add(new Producto(6, "Producto 6", "imagen6.png", 7.72, 1, TipoProducto.ALIMENTO));
+		productos.add(new Producto(7, "Producto 7", "imagen7.png", 8.35, 1, TipoProducto.ALIMENTO));
+		
+		
+		modeloCarrito = new ModeloCarroCompra(productos);
+		tabla = new JTable(modeloCarrito);
+		tabla.setDefaultRenderer(Object.class, new RendererCarroCompra());
+		tabla.setRowHeight(35);
+		tablaScroll = new JScrollPane(tabla);
+		
 		btnEliminar = new JButton("Eliminar del carrito");
 		btnComprar = new JButton("Comprar");
 		btnVaciar = new JButton("Vaciar carrito");
+		
+		panelMedio = new JPanel();
+		panelArriba = new JPanel();
+		panelAbajo = new JPanel();
 		
 		btnVaciar.addActionListener(new ActionListener() {
 			
@@ -69,14 +95,12 @@ public class VentanaCarroCompra extends JFrame{
 			}
 		});
 		
-		JScrollPane carritoScrollPane = new JScrollPane(lstCarrito);
-		JPanel panelInferior = new JPanel();
-		panelInferior.add(btnVaciar);
-		panelInferior.add(btnEliminar);
-		panelInferior.add(btnComprar);
+		panelAbajo.add(btnVaciar);
+		panelAbajo.add(btnEliminar);
+		panelAbajo.add(btnComprar);
 		
-		cp.add(carritoScrollPane, BorderLayout.CENTER);
-		cp.add(panelInferior, BorderLayout.SOUTH);
+		cp.add(tablaScroll);
+		cp.add(panelAbajo, BorderLayout.SOUTH);
 		
 		ImageIcon iconoPrincipal = new ImageIcon("resources/iconos/iconoGAO.png");
 		iconoPrincipal = new ImageIcon(iconoPrincipal.getImage().getScaledInstance(207, 207, Image.SCALE_SMOOTH));
@@ -85,8 +109,18 @@ public class VentanaCarroCompra extends JFrame{
 		
 		this.setTitle("Carrito de la Compra");
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.setSize(400, 300);
+		this.setBounds(680, 80, 500, 400);
 		this.setVisible(false);
+	}
+	
+	
+	public void actualizarCarrito() {
+		tabla.setModel(new ModeloCarroCompra(productos));//cambiar por  jModeloCarro.setModel(new ModeloCarroCompra(productosEnCesta));
+		RendererCarroCompra compraRenderer = new RendererCarroCompra();
+
+		for (int i=0; i<tabla.getColumnModel().getColumnCount(); i++) {
+			tabla.getColumnModel().getColumn(i).setCellRenderer(compraRenderer);
+		}	
 	}
 
 }
