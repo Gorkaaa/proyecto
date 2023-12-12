c287c4e89023961f7e608968b73f541be1240a96
Gorkaaa <gorka.bidaurratzaga@opendeusto.es>
2023-12-12 10:31:34 +0100

    Creada la clase ModeloCarroCompra con sus atributos y metodos

--- gaoMarket/src/gui/ModeloCarroCompra.java
+++ gaoMarket/src/gui/ModeloCarroCompra.java
@@ -1,5 +1,70 @@
 package gui;
 
-public class ModeloCarroCompra {
+import java.util.Arrays;
+import java.util.List;
 
+import javax.swing.table.DefaultTableModel;
+
+import domain.GestorMarket;
+import domain.Producto;
+
+public class ModeloCarroCompra extends DefaultTableModel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected GestorMarket gestor;
+	protected List<Producto> productos;
+	protected final List<String> cabeceras = Arrays.asList("Producto", "Nombre", "Precio", "Cantidad", "Total");
+			
+	
+	public ModeloCarroCompra(List<Producto> productos) {
+		this.productos = productos;
+	}
+	
+	
+	@Override
+	public String getColumnName(int column) {
+		return cabeceras.get(column);
+	}
+	
+	@Override
+	public int getRowCount() {
+		if (productos != null) {
+			return productos.size();
+		} else {
+			return 0;
+		}
+	}
+	
+	@Override
+	public int getColumnCount() {
+		return cabeceras.size();
+	}
+	
+	@Override
+	public boolean isCellEditable(int row, int column) {
+		if (row == 3) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
+	@Override
+	public Object getValueAt(int row, int column) {
+		
+		Producto p = productos.get(row);
+		switch (column) {
+			case 0: return p.getImagen();
+			case 1: return p.getNombre();
+			case 2: return Double.valueOf(p.getPrecio());
+			case 3: return Integer.valueOf(p.getCantidad());
+			case 4: return Double.valueOf((Double) (p.getPrecio()*p.getCantidad()));
+			default: return null;
+		}
+	}
 }
