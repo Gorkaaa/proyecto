2519915466ca90bc9ad118e3aaa4d7009d64a802
Gorkaaa <gorka.bidaurratzaga@opendeusto.es>
2024-10-01 18:45:28 +0200

    Ahora si intentamos cerrar el programa desde la ventana principal aparecera la opcion de si desea cerrar por completo el programa.

--- gaoMarket/src/gui/VentanaPrincipal.java
+++ gaoMarket/src/gui/VentanaPrincipal.java
@@ -254,15 +254,20 @@ public class VentanaPrincipal extends JFrame {
 		iconoPrincipal = new ImageIcon(iconoPrincipal.getImage().getScaledInstance(207, 207, Image.SCALE_SMOOTH));
 			
 		this.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosed(WindowEvent e) {
-            	gestor.getGestorBD().disconnect();
-            	logger.info("Programa finalizado");
+			@Override
+			public void windowClosing(WindowEvent e) {
+
+				int confirm = JOptionPane.showConfirmDialog(null, "¿Deseas cerrar el programa por completo?", "Confirmar cierre", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+				if (confirm == JOptionPane.YES_OPTION) {
+					gestor.getGestorBD().disconnect();
+					logger.info("Programa finalizado");
+					System.exit(0);
+                }
             }
 
 			@Override
 			public void windowOpened(WindowEvent e) {
-				
 				
 			}
 
@@ -272,7 +277,7 @@ public class VentanaPrincipal extends JFrame {
 		this.setIconImage(iconoPrincipal.getImage());
 		
 		this.setTitle("GAO Market");
-		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cambiar a DISPOSE_ON_CLOSE por si el usuario se equivoca, que no cierre todo el programa.
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		this.setBounds(85, 30, 1400, 730);
 		this.setVisible(false);
 		logger.info("Progrma iniciado");
